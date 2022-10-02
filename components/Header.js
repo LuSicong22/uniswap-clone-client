925312be51f3afcee90e50dbfd538a3d1f1f913e
simon <simon.lu@tron.network>
2022-10-02 17:20:26 +0800

    feat:connect wallet

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Image from "next/image";
 import { FiArrowUpRight } from "react-icons/fi";
 import { AiOutlineDown } from "react-icons/ai";
 import { HiOutlineDotsVertical } from "react-icons/hi";
 import ethLogo from "../assets/eth.png";
 import uniswapLogo from "../assets/uniswap.png";
+import { TransactionContext } from "../context/TransactionContext";
 
 const style = {
   wrapper: `p-4 w-screen flex justify-between items-center`,
@@ -23,6 +24,10 @@ const style = {
 
 const Header = () => {
   const [selectedNav, setSelectedNav] = useState("Swap");
+  const { connectWallet, currentAccount } = useContext(TransactionContext);
+
+  console.log({ connectWallet, currentAccount });
+
   return (
     <div className={style.wrapper}>
       <div className={style.headerLogo}>
@@ -74,14 +79,21 @@ const Header = () => {
           <AiOutlineDown />
         </div>
       </div>
-      <div
-        onClick={() => connectWallet()}
-        className={`${style.button} ${style.buttonPadding}`}
-      >
-        <div className={`${style.buttonAccent} ${style.buttonPadding}`}>
-          Connect Wallet
+      {currentAccount ? (
+        <div className={`${style.button} ${style.buttonPadding}`}>
+          <div className={style.buttonTextContainer}>0x13...98</div>
         </div>
-      </div>
+      ) : (
+        <div
+          onClick={() => connectWallet()}
+          className={`${style.button} ${style.buttonPadding}`}
+        >
+          <div className={`${style.buttonAccent} ${style.buttonPadding}`}>
+            Connect Wallet
+          </div>
+        </div>
+      )}
+
       <div className={`${style.button} ${style.buttonPadding}`}>
         <div className={style.buttonIconContainer}>
           <HiOutlineDotsVertical />
